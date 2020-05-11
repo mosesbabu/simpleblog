aa7869ff97ee71fbf8b43b315509ea8396768484
moselamar <44118179+moselamar@users.noreply.github.com>
2020-05-11 21:03:35 +0300

    create request for qutes

---
+++ request.py
@@ -0,0 +1,13 @@
+import requests
+from config import Config
+from .models import Quotes
+quotes_url = Config.QUOTES_URL
+def getQuotes():
+    
+   random_quote = requests.get(quotes_url)
+   new_quote = random_quote.json()
+   author = new_quote.get("author")
+   quote = new_quote.get("quote")
+   permalink = new_quote.get("permalink")
+   quote_object = Quotes(author,quote,permalink)
+   return quote_object
