589cf6da30092eebd68ec1b177ade57a111689a4
moselamar <44118179+moselamar@users.noreply.github.com>
2020-05-11 21:16:40 +0300

    added quotes requests

--- request.py
+++ request.py
@@ -1,8 +1,21 @@
-import requests
+import urllib.request,json
 from config import Config
-from .models import Quotes
-quotes_url = Config.QUOTES_URL
-def getQuotes():
+
+quotes_url = Config['QUOTES_URL']
+def get_quotes(id):
+    
+    with urllib.request.urlopen(quotes_url) as url:
+        quotes_data = url.read()
+        quotes_response = json.loads(quotes_data)
+        
+        quote_object=None
+        if quotes_response:
+            id = quotes_response.get('id')
+            author = quotes_response.get('author')
+            quote = quotes_response.get('quote')
+            permalink = quotes_response.get('permalink')
+    
+    
     
    random_quote = requests.get(quotes_url)
    new_quote = random_quote.json()
