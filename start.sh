812362686ff95089fde18aea5d0c5736f341dd99
moselamar <44118179+moselamar@users.noreply.github.com>
2020-05-11 08:34:25 +0300

    added picture for posts in template

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ export SECRET_KEY="flasksecret"
 export MAIL_USERNAME="mosesarara@gmail.com"
 export MAIL_PASSWORD="softwa"
 
-export DATABASE_URL='postgresql+psycopg2://postgres:software112@localhost/blog'
+export DATABASE_URL='postgresql+psycopg2://postgres:software2@localhost/blog'
 
 python3.6 manage.py server
